9c52e626e2c8c851ba90df254a973b8359406e58
JacobKilburn <113478052+JacobKilburn@users.noreply.github.com>
2025-09-18 20:57:54 -0400

    tested vecadd and image2gray

--- Lab1/lab1-vector/kernel.cu
+++ Lab1/lab1-vector/kernel.cu
@@ -6,41 +6,42 @@
  *cr
  ******************************************************************************/
 
-__global__ void vecAddKernel(float *A, float *B, float *C, int n)
-{
-	// Calculate global thread index based on the block and thread indices ----
-	//INSERT KERNEL CODE HERE
-
-	// Use global index to determine which elements to read, add, and write ---
-	//INSERT KERNEL CODE HERE, BE CAREFUL FOR CORNER CASE!!!
-	
-	// int i = threadIdx.x;
-	// C[i] = A[i] + B[i];
-
-	//what is n used for? For loop?
-
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	if (i<n) C[i] = A[i] + B[i];
-}
-
-__global__ void image2grayKernel(float *in, float *out, int height, int width)
-{
-	// Calculate global thread index based on the block and thread indices ----
-	//INSERT KERNEL CODE HERE
-	
-	// Use global index to determine which elements to read, add, and write ---
-	//INSERT KERNEL CODE HERE, BE CAREFUL FOR CORNER CASE!!!
-	
-	int x = blockIdx.x * blockDim.x + threadIdx.x; // column
-    int y = blockIdx.y * blockDim.y + threadIdx.y; // row
-
-    if (x < width && y < height) {
-        int i = y * width + x;
-        int rgb_idx = i * 3;
-
-        float r = in[rgb_idx];
-        float g = in[rgb_idx + 1];
-        float b = in[rgb_idx + 2];
-
-        out[i] = 0.114f * r + 0.587f * g + 0.299f * b;
-}
+ __global__ void vecAddKernel(float *A, float *B, float *C, int n)
+ {
+	 // Calculate global thread index based on the block and thread indices ----
+	 //INSERT KERNEL CODE HERE
+ 
+	 // Use global index to determine which elements to read, add, and write ---
+	 //INSERT KERNEL CODE HERE, BE CAREFUL FOR CORNER CASE!!!
+	 
+	 // int i = threadIdx.x;
+	 // C[i] = A[i] + B[i];
+ 
+	 //what is n used for? For loop?
+ 
+	 int i = blockIdx.x * blockDim.x + threadIdx.x;
+	 if (i<n) C[i] = A[i] + B[i];
+ }
+ 
+ __global__ void image2grayKernel(float *in, float *out, int height, int width)
+ {
+	 // Calculate global thread index based on the block and thread indices ----
+	 //INSERT KERNEL CODE HERE
+	 
+	 // Use global index to determine which elements to read, add, and write ---
+	 //INSERT KERNEL CODE HERE, BE CAREFUL FOR CORNER CASE!!!
+	 
+	 int x = blockIdx.x * blockDim.x + threadIdx.x; // column
+	 int y = blockIdx.y * blockDim.y + threadIdx.y; // row
+ 
+	 if (x < width && y < height) {
+		 int i = y * width + x;
+		 int rgb_idx = i * 3;
+ 
+		 float r = in[rgb_idx];
+		 float g = in[rgb_idx + 1];
+		 float b = in[rgb_idx + 2];
+ 
+		 out[i] = 0.144f * r + 0.587f * g + 0.299f * b;
+	 }
+ }
--- Lab1/lab1-vector/vector_add.cu
+++ Lab1/lab1-vector/vector_add.cu
@@ -5,142 +5,143 @@
  *cr                         All Rights Reserved
  *cr
  ******************************************************************************/
-#include <stdio.h>
-#include "support.h"
-#include "kernel.cu"
-
-void verify(float *A, float *B, float *C, int n)
-{
-	const float relativeTolerance = 1e-6;
-	for (int i = 0; i < n; i++)
-	{
-		float sum = A[i] + B[i];
-		float relativeError = (sum - C[i]) / sum;
-		if (relativeError > relativeTolerance ||
-			relativeError < -relativeTolerance)
-		{
-			printf("TEST FAILED\n\n");
-			exit(0);
-		}
-	}
-
-	printf("TEST PASSED\n\n");
-}
-
-int main(int argc, char **argv)
-{
-	Timer timer;
-	cudaError_t cuda_ret;
-
-	// Initialize host variables ----------------------------------------------
-
-	printf("\nSetting up the problem...");
-	fflush(stdout);
-	startTime(&timer);
-
-	unsigned int n;
-	if (argc == 1)
-	{
-		n = 10000;
-	}
-	else if (argc == 2)
-	{
-		n = atoi(argv[1]);
-	}
-	else
-	{
-		printf("\n    Invalid input parameters!"
-			"\n    Usage: ./vector_add       # Vector of size 10,000 is used"
-			"\n    Usage: ./vector_add <m>   # Vector of size m is used"
-			"\n");
-		exit(0);
-	}
-
-	float *A_h = (float*) malloc(sizeof(float) *n);
-	for (unsigned int i = 0; i < n; i++)
-	{
-		A_h[i] = (rand() % 100) / 100.00;
-	}
-
-	float *B_h = (float*) malloc(sizeof(float) *n);
-	for (unsigned int i = 0; i < n; i++)
-	{
-		B_h[i] = (rand() % 100) / 100.00;
-	}
-
-	float *C_h = (float*) malloc(sizeof(float) *n);
-
-	stopTime(&timer);
-	printf("%f s\n", elapsedTime(timer));
-	printf("    Vector size = %u\n", n);
-
-	// Allocate device variables ----------------------------------------------
-	printf("Allocating device variables...");
-	fflush(stdout);
-	startTime(&timer);
-	//INSERT CODE HERE
-	cudaMalloc((void**)&A_d, n * sizeof(float));
-	cudaMalloc((void**)&B_d, n * sizeof(float));
-	cudaMalloc((void**)&C_d, n * sizeof(float));
-	
-	cudaDeviceSynchronize();
-	stopTime(&timer);
-	printf("%f s\n", elapsedTime(timer));
-
-	// Copy host variables to device ------------------------------------------
-	printf("Copying data from host to device...");
-	fflush(stdout);
-	startTime(&timer);
-	//INSERT CODE HERE
-	cudaMemcpy(A_d, A_h, n * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(B_d, B_h, n * sizeof(float), cudaMemcpyHostToDevice);
-
-	cudaDeviceSynchronize();
-	stopTime(&timer);
-	printf("%f s\n", elapsedTime(timer));
-
-	// Launch kernel ----------------------------------------------------------
-	printf("Launching kernel...");
-	fflush(stdout);
-	startTime(&timer);
-	//INSERT CODE HERE
-	int blockSize = 256; //Can be 256, 512, or 1024
-	int gridSize = (n + blockSize - 1) / blockSize;
-
-	vectorAdd<<<gridSize, blockSize>>>(A_d, B_d, C_d, n);
-
-
-	cuda_ret = cudaDeviceSynchronize();
-	if (cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
-	stopTime(&timer);
-	printf("%f s\n", elapsedTime(timer));
-
-	// Copy device variables from host ----------------------------------------
-	printf("Copying data from device to host...");
-	fflush(stdout);
-	startTime(&timer);
-	//INSERT CODE HERE
-	cudaMemcpy(C_h, C_d, n * sizeof(float), cudaMemcpyDeviceToHost);
-
-	cudaDeviceSynchronize();
-	stopTime(&timer);
-	printf("%f s\n", elapsedTime(timer));
-
-	// Verify correctness -----------------------------------------------------
-	printf("Verifying results...");
-	fflush(stdout);
-	verify(A_h, B_h, C_h, n);
-
-
-	// Free memory ------------------------------------------------------------
-	free(A_h);
-	free(B_h);
-	free(C_h);
-	//INSERT CODE HERE
-	cudaFree(A_d);
-	cudaFree(B_d);
-	cudaFree(C_d);
-
-	return 0;
-
-}
+ #include <stdio.h>
+ #include "support.h"
+ #include "kernel.cu"
+ 
+ void verify(float *A, float *B, float *C, int n)
+ {
+	 const float relativeTolerance = 1e-6;
+	 for (int i = 0; i < n; i++)
+	 {
+		 float sum = A[i] + B[i];
+		 float relativeError = (sum - C[i]) / sum;
+		 if (relativeError > relativeTolerance ||
+			 relativeError < -relativeTolerance)
+		 {
+			 printf("TEST FAILED\n\n");
+			 exit(0);
+		 }
+	 }
+ 
+	 printf("TEST PASSED\n\n");
+ }
+ 
+ int main(int argc, char **argv)
+ {
+	 Timer timer;
+	 cudaError_t cuda_ret;
+ 
+	 // Initialize host variables ----------------------------------------------
+ 
+	 printf("\nSetting up the problem...");
+	 fflush(stdout);
+	 startTime(&timer);
+ 
+	 unsigned int n;
+	 if (argc == 1)
+	 {
+		 n = 10000;
+	 }
+	 else if (argc == 2)
+	 {
+		 n = atoi(argv[1]);
+	 }
+	 else
+	 {
+		 printf("\n    Invalid input parameters!"
+			 "\n    Usage: ./vector_add       # Vector of size 10,000 is used"
+			 "\n    Usage: ./vector_add <m>   # Vector of size m is used"
+			 "\n");
+		 exit(0);
+	 }
+ 
+	 float *A_h = (float*) malloc(sizeof(float) *n);
+	 for (unsigned int i = 0; i < n; i++)
+	 {
+		 A_h[i] = (rand() % 100) / 100.00;
+	 }
+ 
+	 float *B_h = (float*) malloc(sizeof(float) *n);
+	 for (unsigned int i = 0; i < n; i++)
+	 {
+		 B_h[i] = (rand() % 100) / 100.00;
+	 }
+ 
+	 float *C_h = (float*) malloc(sizeof(float) *n);
+ 
+	 stopTime(&timer);
+	 printf("%f s\n", elapsedTime(timer));
+	 printf("    Vector size = %u\n", n);
+ 
+	 // Allocate device variables ----------------------------------------------
+	 printf("Allocating device variables...");
+	 fflush(stdout);
+	 startTime(&timer);
+	 //INSERT CODE HERE
+	 float *A_d, *B_d, *C_d;
+	 cudaMalloc((void**)&A_d, n * sizeof(float));
+	 cudaMalloc((void**)&B_d, n * sizeof(float));
+	 cudaMalloc((void**)&C_d, n * sizeof(float));
+	 
+	 cudaDeviceSynchronize();
+	 stopTime(&timer);
+	 printf("%f s\n", elapsedTime(timer));
+ 
+	 // Copy host variables to device ------------------------------------------
+	 printf("Copying data from host to device...");
+	 fflush(stdout);
+	 startTime(&timer);
+	 //INSERT CODE HERE
+	 cudaMemcpy(A_d, A_h, n * sizeof(float), cudaMemcpyHostToDevice);
+	 cudaMemcpy(B_d, B_h, n * sizeof(float), cudaMemcpyHostToDevice);
+ 
+	 cudaDeviceSynchronize();
+	 stopTime(&timer);
+	 printf("%f s\n", elapsedTime(timer));
+ 
+	 // Launch kernel ----------------------------------------------------------
+	 printf("Launching kernel...");
+	 fflush(stdout);
+	 startTime(&timer);
+	 //INSERT CODE HERE
+	 int blockSize = 1024; //Can be 256, 512, or 1024
+	 int gridSize = (n + blockSize - 1) / blockSize;
+ 
+	 vecAddKernel<<<gridSize, blockSize>>>(A_d, B_d, C_d, n);
+ 
+ 
+	 cuda_ret = cudaDeviceSynchronize();
+	 if (cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
+	 stopTime(&timer);
+	 printf("%f s\n", elapsedTime(timer));
+ 
+	 // Copy device variables from host ----------------------------------------
+	 printf("Copying data from device to host...");
+	 fflush(stdout);
+	 startTime(&timer);
+	 //INSERT CODE HERE
+	 cudaMemcpy(C_h, C_d, n * sizeof(float), cudaMemcpyDeviceToHost);
+ 
+	 cudaDeviceSynchronize();
+	 stopTime(&timer);
+	 printf("%f s\n", elapsedTime(timer));
+ 
+	 // Verify correctness -----------------------------------------------------
+	 printf("Verifying results...");
+	 fflush(stdout);
+	 verify(A_h, B_h, C_h, n);
+ 
+ 
+	 // Free memory ------------------------------------------------------------
+	 free(A_h);
+	 free(B_h);
+	 free(C_h);
+	 //INSERT CODE HERE
+	 cudaFree(A_d);
+	 cudaFree(B_d);
+	 cudaFree(C_d);
+ 
+	 return 0;
+ 
+ }
